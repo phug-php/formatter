cd121d98d0ef30c3a97cbcf704301b3fabfecd03
KyleKatarn <kylekatarnls@gmail.com>
2016-12-16 16:44:49 +0100

    Increase phpcs line length limit because DOCTYPE constant cannot be split

--- src/Phug/Formatter/Element/MarkupElement.php
+++ src/Phug/Formatter/Element/MarkupElement.php
@@ -2,6 +2,7 @@
 
 namespace Phug\Formatter\Element;
 
+use Phug\Ast\NodeInterface;
 use Phug\Formatter\AbstractElement;
 use Phug\Util\Partial\AttributeTrait;
 use Phug\Util\Partial\NameTrait;
@@ -12,8 +13,12 @@ class MarkupElement extends AbstractElement
     use AttributeTrait;
     use NameTrait;
 
-    public function __construct($name, NodeInterface $parent = null, SplObjectStorage $attributes = null, array $children = null)
-    {
+    public function __construct(
+        $name,
+        NodeInterface $parent = null,
+        SplObjectStorage $attributes = null,
+        array $children = null
+    ) {
         parent::__construct($parent, $children);
 
         $this->setName($name);
