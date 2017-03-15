0fff7c0a08653ff3e6a834e662b7fce6d5a7e26c
KyleKatarn <kylekatarnls@gmail.com>
2017-03-15 15:43:22 +0100

    Fix variable element construct to match other AST

--- src/Phug/Formatter/Element/VariableElement.php
+++ src/Phug/Formatter/Element/VariableElement.php
@@ -3,6 +3,7 @@
 namespace Phug\Formatter\Element;
 
 use Phug\Formatter\AbstractElement;
+use Phug\Util\UnorderedArguments;
 
 class VariableElement extends AbstractElement
 {
@@ -16,10 +17,26 @@ class VariableElement extends AbstractElement
      */
     protected $expression;
 
-    public function __construct(CodeElement $variable = null, ExpressionElement $expression = null)
+    public function __construct()
     {
-        $variable && $this->setVariable($variable);
-        $expression && $this->setExpression($expression);
+        $arguments = new UnorderedArguments(func_get_args());
+
+        $variable = $arguments->optional(CodeElement::class);
+        $expression = $arguments->optional(ExpressionElement::class);
+        $parent = $arguments->optional(NodeInterface::class);
+        $children = $arguments->optional('array');
+
+        $arguments->noMoreDefinedArguments();
+
+        if ($variable !== null) {
+            $this->setVariable($variable);
+        }
+
+        if ($expression !== null) {
+            $this->setExpression($expression);
+        }
+
+        parent::__construct($parent, $children);
     }
 
     public function setVariable(CodeElement $variable)
