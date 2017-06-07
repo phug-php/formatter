d2148f77a33d8a95c72da3c380532c1e15a266bf
KyleKatarn <kylekatarnls@gmail.com>
2017-06-07 15:49:28 +0200

    Update FormatInterface

--- src/Phug/Formatter/FormatInterface.php
+++ src/Phug/Formatter/FormatInterface.php
@@ -13,5 +13,15 @@ interface FormatInterface
 
     public function format($element);
 
+    public function setFormatter(Formatter $formatter);
+
+    public function removeElementHandler($className);
+
+    public function setElementHandler($className, callable $handler);
+
+    public function removePhpTokenHandler($phpTokenId);
+
+    public function setPhpTokenHandler($phpTokenId, $handler);
+
     public function __invoke(ElementInterface $element);
 }
