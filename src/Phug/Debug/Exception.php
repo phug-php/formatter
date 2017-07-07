80a73c18f82c073e73e3980d4fbf1f258745b92f
KyleKatarn <kylekatarnls@gmail.com>
2017-07-07 13:57:34 +0200

    Use PugFileLocationInterface for Debug\Exception

--- src/Phug/Debug/Exception.php
+++ src/Phug/Debug/Exception.php
@@ -2,52 +2,18 @@
 
 namespace Phug\Debug;
 
-class Exception extends \Exception
+use Phug\Util\Partial\PugFileLocationTrait;
+use Phug\Util\PugFileLocationInterface;
+
+class Exception extends \Exception implements PugFileLocationInterface
 {
-    /**
-     * @var string
-     */
-    protected $pugFile;
-
-    /**
-     * @var int
-     */
-    protected $pugLine;
-
-    /**
-     * @var int
-     */
-    protected $pugOffset;
+    use PugFileLocationTrait;
 
     public function __construct($message, $code, $previous, $file, $line, $offset)
     {
-        $this->pugFile = $file;
-        $this->pugLine = $line;
-        $this->pugOffset = $offset;
+        $this->setPugFile($file);
+        $this->setPugLine($line);
+        $this->setPugOffset($offset);
         parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * @return string
-     */
-    public function getPugFile()
-    {
-        return $this->pugFile;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPugLine()
-    {
-        return $this->pugLine;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPugOffset()
-    {
-        return $this->pugOffset;
     }
 }
