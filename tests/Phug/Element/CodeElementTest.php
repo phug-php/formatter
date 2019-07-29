23bbb93f171df13edf830494dd873115656ca2aa
KyleKatarn <kylekatarnls@gmail.com>
2019-07-29 11:25:23 +0200

    Implement pre/post hooks on CodeElement
    Required for:
    https://github.com/phug-php/phug/issues/41

--- tests/Phug/Element/CodeElementTest.php
+++ tests/Phug/Element/CodeElementTest.php
@@ -3,6 +3,7 @@
 namespace Phug\Test\Element;
 
 use PHPUnit\Framework\TestCase;
+use Phug\Formatter;
 use Phug\Formatter\Element\CodeElement;
 
 /**
@@ -18,5 +19,43 @@ class CodeElementTest extends TestCase
         $foo = new CodeElement('$foo');
 
         self::assertSame('$foo', $foo->getValue());
+    }
+
+    public function testHooks()
+    {
+        $code = new CodeElement('$foo = 9;');
+        $code->setPreHook('$__eachScopeVariables = [\'foo\' => isset($foo) ? $foo : null];');
+        $code->setPostHook('extract($__eachScopeVariables);');
+        $formatter = new Formatter();
+
+        ob_start();
+        $php = $formatter->format($code);
+        eval('?>'.$formatter->formatDependencies().$php);
+        ob_end_clean();
+
+        self::assertNull($foo);
+
+        $code = new CodeElement('$foo = 9;');
+        $formatter = new Formatter();
+
+        ob_start();
+        $php = $formatter->format($code);
+        eval('?>'.$formatter->formatDependencies().$php);
+        ob_end_clean();
+
+        self::assertSame(9, $foo);
+
+        $code = new CodeElement('$foo = 9;');
+        $code->setPreHook('$__eachScopeVariables = [\'foo\' => isset($foo) ? $foo : null];');
+        $code->setPostHook('extract($__eachScopeVariables);');
+        $formatter = new Formatter();
+
+        ob_start();
+        $foo = 42;
+        $php = $formatter->format($code);
+        eval('?>'.$formatter->formatDependencies().$php);
+        ob_end_clean();
+
+        self::assertSame(42, $foo);
     }
 }
